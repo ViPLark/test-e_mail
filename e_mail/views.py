cfe44571341c1ffb95a6b503676ff752b08b147e
Merzix <merzix@mail.ru>
2019-10-06 16:06:21 +0300

    Add the ability to mark an email as read

--- e_mail/views.py
+++ e_mail/views.py
@@ -1,7 +1,16 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
 
 from .models import Inbox, Sent
 from .serializers import EmailSerializer, InboxSerializer, SentSerializer
+
+
+class DetailAPIView(RetrieveAPIView):
+    def get(self, request, *args, **kwargs):
+        if 'new' in request.GET:
+            instance = self.get_object()
+            instance.is_new = request.GET['new']
+            instance.save()
+        return super().get(request, *args, **kwargs)
 
 
 class InboxListView(ListAPIView):
@@ -11,7 +20,7 @@ class InboxListView(ListAPIView):
         return Inbox.objects.filter(user=self.request.user)
 
 
-class InboxDetailView(RetrieveDestroyAPIView):
+class InboxDetailView(DetailAPIView, DestroyAPIView):
     serializer_class = InboxSerializer
 
     def get_queryset(self):
@@ -25,7 +34,7 @@ class SentListView(ListAPIView):
         return Sent.objects.filter(user=self.request.user)
 
 
-class SentDetailView(RetrieveDestroyAPIView):
+class SentDetailView(DetailAPIView, DestroyAPIView):
     serializer_class = SentSerializer
 
     def get_queryset(self):
