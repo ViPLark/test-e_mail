4c17313e79b5abd735b4990da57b1a1be6982e58
Merzix <merzix@mail.ru>
2019-10-06 15:25:18 +0300

    Update models

--- e_mail/models.py
+++ e_mail/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 
 class Email(models.Model):
-    from_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='sent_email_set')
-    to_users = models.ManyToManyField(User, related_name='inbox_email_set')
+    from_user = models.ForeignKey(User, verbose_name='from', on_delete=models.PROTECT, related_name='sent_email_set')
+    to_users = models.ManyToManyField(User, verbose_name='to', related_name='inbox_email_set')
     title = models.CharField(max_length=500)
     text = models.TextField()
 
@@ -17,33 +17,26 @@ class Email(models.Model):
         return self.title
 
 
-class Inbox(models.Model):
+class Catalog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.ForeignKey(Email, on_delete=models.DO_NOTHING)
+    is_new = models.BooleanField(default=True)
 
     # service
-    received_dt = models.DateTimeField(auto_now_add=True)
+    add_dt = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.email.title
+
+
+class Inbox(Catalog):
     class Meta:
         verbose_name_plural = 'Inbox emails'
 
-    def __str__(self):
-        return self.email.title
 
-
-class Sent(models.Model):
-    email = models.ForeignKey(Email, on_delete=models.DO_NOTHING)
-    to_user = models.ForeignKey(User, on_delete=models.PROTECT)
-
-    # service
-    send_dt = models.DateTimeField(auto_now_add=True)
-
+class Sent(Catalog):
     class Meta:
         verbose_name_plural = 'Sent emails'
-
-    def __str__(self):
-        return self.email.title
-
-    @property
-    def is_sent(self):
-        return self.send_dt is not None
